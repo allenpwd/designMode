057e96ef053210b9531f637a8719160bd42b152c
pwdan <994266136@qq.com>
2020-01-19 18:19:12 +0800

    代理模式补充

--- src/main/java/pwd/allen/proxy/jdk/ProxyFactory.java
+++ src/main/java/pwd/allen/proxy/jdk/ProxyFactory.java
@@ -12,7 +12,7 @@ import java.lang.reflect.Proxy;
  * @author 门那粒沙
  * @create 2019-07-06 18:38
  **/
-public class ProxyFactory {
+public class ProxyFactory implements InvocationHandler {
     //维护一个目标对象
     private Object target;
     public ProxyFactory(Object target){
@@ -22,25 +22,34 @@ public class ProxyFactory {
     /**
      * 给目标对象生成代理对象
      * newProxyInstance方法是在Proxy类中是静态方法,且接收的三个参数依次为:
-     * ClassLoader loader,:指定当前目标对象使用类加载器,获取加载器的方法是固定的
-     * Class<?>[] interfaces,:目标对象实现的接口的类型,使用泛型方式确认类型
-     * InvocationHandler h:事件处理,执行目标对象的方法时,会触发事件处理器的方法,会把当前执行目标对象的方法作为参数传入
+     *  ClassLoader loader,:指定当前目标对象使用类加载器,获取加载器的方法是固定的
+     *  Class<?>[] interfaces,:目标对象实现的接口的类型,使用泛型方式确认类型
+     *  InvocationHandler h:事件处理,执行目标对象的方法时,会触发事件处理器的方法,会把当前执行目标对象的方法作为参数传入
      */
     public Object getProxyInstance(){
         return Proxy.newProxyInstance(
                 target.getClass().getClassLoader(),
                 target.getClass().getInterfaces(),
-                new InvocationHandler() {
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        System.out.println("方法执行前:" + method.getName());
-                        //执行目标对象方法
-                        Object returnValue = method.invoke(target, args);
-                        System.out.println("方法执行后：" + method.getName());
-                        return returnValue;
-                    }
-                }
+                this
         );
+    }
+
+    /**
+     * 事件处理,执行目标对象的方法时,会触发事件处理器的方法,会把当前执行目标对象的方法作为参数传入
+     *
+     * @param proxy 代理对象
+     * @param method 代理的目标对象的方法
+     * @param args 方法参数
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("方法执行前:" + method.getName());
+        //执行目标对象方法
+        Object returnValue = method.invoke(target, args);
+        System.out.println("方法执行后：" + method.getName());
+        return returnValue;
     }
 
     public static void main(String[] args) {
